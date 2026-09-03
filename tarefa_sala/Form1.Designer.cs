35eeddb5c69ddc400697d543362df5e53ff56bd9
fabio.vsmoura <fabio.vsmoura@gmail.com>
2026-09-03 17:40:26 -0300

    Mais uma label

---
+++ tarefa_sala/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace tarefa_sala
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_conexao = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btn_conexao
+            // 
+            btn_conexao.Location = new Point(633, 12);
+            btn_conexao.Name = "btn_conexao";
+            btn_conexao.Size = new Size(155, 23);
+            btn_conexao.TabIndex = 0;
+            btn_conexao.Text = "Teste conexão";
+            btn_conexao.UseVisualStyleBackColor = true;
+            btn_conexao.Click += btn_conexao_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Tela de cadastro";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(45, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Tela de cadastro";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btn_conexao);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_conexao;
+        private Label label1;
+        private Label label2;
+    }
+}
